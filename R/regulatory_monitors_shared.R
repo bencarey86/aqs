e7756164310e54f67b87f4e5444eb6decf414d19
Ben Carey <george.carey@yale.edu>
2025-03-13 09:52:48 -0400

    add more functions

--- R/regulatory_monitors_shared.R
+++ R/regulatory_monitors_shared.R
@@ -11,6 +11,15 @@
     excel_file_path
 }
 
+.import_regulatory_monitor_data <- function(excel_file_path, sheet, skip) {
+    readxl::read_excel(
+        path = excel_file_path,
+        sheet = sheet,
+        skip = skip
+    ) |>
+        janitor::clean_names()
+}
+
 .select_cols <- function(df, col_patterns) {
     df |>
         dplyr::select(matches(col_patterns))
@@ -23,5 +32,26 @@
         df$aqs_site_id <- as.character(df$aqs_site_id)
         df$aqs_site_id <- stringr::str_pad(df$aqs_site_id, 9, side = "left", pad = "0")
     }
+    df
+}
+
+.convert_site_id_poc_column_types <- function(df) {
+    df$aqs_site_id <- as.character(df$aqs_site_id)
+    df$aqs_site_id <- stringr::str_pad(df$aqs_site_id, 9, side = "left", pad = "0")
+    df$poc <- as.integer(df$poc)
+    df
+}
+
+.combine_standards <- function(df_1, df_2) {
+    # Ensure valid_dv is numeric in both DFs before binding
+    df_1$valid_dv <- as.numeric(df_1$valid_dv)
+    df_2$valid_dv <- as.numeric(df_2$valid_dv)
+    combined_df <- rbind(df_1, df_2)
+}
+
+.drop_rows_all_na <- function(df) {
+    rows_to_keep <- rowSums(!is.na(df)) > 0
+    df <- df[rows_to_keep, ]
+    rownames(df) <- NULL
     df
 }
--- tests/testthat/test-regulatory_monitors_shared.R
+++ tests/testthat/test-regulatory_monitors_shared.R
@@ -1,3 +1,62 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("Standards are combined correctly", {
+  df_1 <- data.frame(
+    valid_dv = c("1", "2", "3")
+  )
+  df_2 <- data.frame(
+    valid_dv = c("4", "5", "6")
+  )
+  expected_df <- data.frame(
+    valid_dv = c(1, 2, 3, 4, 5, 6)
+  )
+  expect_equal(
+    .combine_standards(df_1, df_2),
+    expected_df
+  )
+})
+
+test_that("Site ID and POC column types are converted correctly", {
+  df_1 <- data.frame(
+    aqs_site_id = c(1, 2, 3),
+    poc = c(4, 5, 6)
+  )
+  df_2 <- aqs_site_id <- data.frame(
+    aqs_site_id = c("000000001", "000000002", "000000003"),
+    poc = c("4", "5", "6")
+  )
+  df_3 <- data.frame(
+    aqs_site_id = c(1, 2, 3),
+    poc = c("4", "5", "6")
+  )
+  df_4 <- data.frame(
+    aqs_site_id = c("000000001", "000000002", "000000003"),
+    poc = c(4, 5, 6)
+  )
+  expected_df <- data.frame(
+    aqs_site_id = c("000000001", "000000002", "000000003"),
+    poc = c(4, 5, 6)
+  )
+  df_list <- list(df_1, df_2, df_3, df_4)
+  purrr::map(df_list, function(df) {
+    expect_equal(
+      .convert_site_id_poc_column_types(df),
+      expected_df
+    )
+  })
+})
+
+test_that("drop_rows_all_na drops only rows with all NA values", {
+  df <- data.frame(
+    a = c(1, NA, 3),
+    b = c(NA, NA, NA),
+    c = c(NA, NA, 3)
+  )
+  expected_df <- data.frame(
+    a = c(1, 3),
+    b = c(NA, NA),
+    c = c(NA, 3)
+  )
+  expect_equal(
+    .drop_rows_all_na(df),
+    expected_df
+  )
 })
