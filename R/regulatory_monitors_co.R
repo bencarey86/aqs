292a903f91047a598b6ccbbe5baf8a0d7b02cdc7
Ben Carey <george.carey@yale.edu>
2025-03-13 09:52:18 -0400

    refactor to make more modular, improve testing coverage

--- R/regulatory_monitors_co.R
+++ R/regulatory_monitors_co.R
@@ -1,3 +1,4 @@
+# Set up -----------------------------------------------------------------------
 .define_co_url <- function(year) {
     url <- switch(as.character(year),
         "2023" = "https://www.epa.gov/system/files/documents/2024-06/co_designvalues_2022_2023_final_05_07_24.xlsx",
@@ -19,6 +20,21 @@
         "2007" = "https://www.epa.gov/sites/default/files/2016-09/dv_co_2005_2007.xls",
         "2006" = "https://www.epa.gov/sites/default/files/2016-09/dv_co_2004_2006.xls",
         "2005" = "https://www.epa.gov/sites/default/files/2016-09/dv_ozone_2003_2005.xls"
+    )
+}
+
+.define_co_file_setup <- function(year, directory_name) {
+    temp_directory <- .create_temp_subdirectory(directory_name)
+    url <- .define_co_url(year)
+    excel_file_path <- .download_regulatory_monitor_data(
+        year = year,
+        url = url,
+        temp_directory = temp_directory
+    )
+    list(
+        temp_directory = temp_directory,
+        url = url,
+        excel_file_path = excel_file_path
     )
 }
 
@@ -53,13 +69,42 @@
     )
 }
 
-.import_co_data <- function(year, excel_file_path, sheet) {
-    readxl::read_excel(
-        path = excel_file_path,
-        sheet = sheet,
-        skip = ifelse(year == 2013, 2, 3)
-    ) |>
-        janitor::clean_names()
+.define_co_excel_sheets <- function(year) {
+    if (year %in% 2012:2013) {
+        co_8h_sites <- .define_co_8h_sites_sheet(year = year)
+        co_1h_sites <- .define_co_1h_sites_sheet(year = year)
+        co_8h_monitors <- .define_co_8h_monitors_sheet(year = year)
+        co_1h_monitors <- .define_co_1h_monitors_sheet(year = year)
+        list(
+            co_8h_sites = co_8h_sites,
+            co_1h_sites = co_1h_sites,
+            co_8h_monitors = co_8h_monitors,
+            co_1h_monitors = co_1h_monitors
+        )
+    } else {
+        co_8h_monitors <- .define_co_8h_monitors_sheet(year = year)
+        co_1h_monitors <- .define_co_1h_monitors_sheet(year = year)
+        list(
+            co_8h_monitors = co_8h_monitors,
+            co_1h_monitors = co_1h_monitors
+        )
+    }
+}
+
+.define_co_excel_rows_to_skip <- function(year) {
+    if (year %in% c(2012, 2014:2023)) {
+        skip <- 3
+    } else if (year == 2013) {
+        skip <- 2
+    }
+}
+
+.define_co_monitor_column_patterns <- function() {
+    "^site$|aqs_site_id|poc|^valid|x20[0-9]{2}_[0-9]_hour_design_value"
+}
+
+.define_co_site_column_patterns <- function(year) {
+    ifelse(year == 2012, "^site$|2012", "^site$|2013")
 }
 
 #' Define column patterns to use for selecting columns
@@ -69,102 +114,204 @@
 #' and the level of data (site or monitor).
 #'
 #' @param year The year of the data
-#' @param level The level of data (site or monitor)
-#' @return A character vector of column patterns
+#' @return A list of column patterns
 #' @keywords internal
-.define_co_column_patterns <- function(year, level = c("site", "monitor")) {
-    if (level == "monitor") {
-        col_patterns <- "^site$|aqs_site_id|poc|^valid|x20[0-9]{2}_[0-9]_hour_design_value"
-    } else if (level == "site") {
-        col_patterns <- ifelse(year == 2012, "^site$|2012", "^site$|2013")
+.define_co_column_patterns <- function(year) {
+    if (year %in% 2014:2023) {
+        list(monitors = .define_co_monitor_column_patterns())
+    } else if (year %in% 2012:2013) {
+        list(
+            sites = .define_co_site_column_patterns(year = year),
+            monitors = .define_co_monitor_column_patterns()
+        )
     }
 }
 
+.set_up_co_processing <- function(year) {
+    file_definitions <- .define_co_file_setup(year = year, directory_name = "co_design_values")
+    sheets <- .define_co_excel_sheets(year = year)
+    skip <- .define_co_excel_rows_to_skip(year = year)
+    column_patterns <- .define_co_column_patterns(year = year)
+    list(
+        file_definitions = file_definitions,
+        sheets = sheets,
+        skip = skip,
+        column_patterns = column_patterns
+    )
+}
+
+# Preliminary data processing ---------------------------------------------------
 .rename_co_columns <- function(df) {
     colnames(df)[grep("site", colnames(df))] <- "aqs_site_id"
     colnames(df)[grep("design_value", colnames(df))] <- "valid_dv"
     df
 }
 
-.combine_co_standards <- function(df_8h, df_1h) {
-    # Ensure valid_dv is numeric in both DFs before binding
-    df_8h$valid_dv <- as.numeric(df_8h$valid_dv)
-    df_1h$valid_dv <- as.numeric(df_1h$valid_dv)
-    combined_df <- rbind(df_8h, df_1h)
+.get_initial_co_monitors <- function(excel_file_path, sheet, skip, column_patterns) {
+    .import_regulatory_monitor_data(
+        excel_file_path = excel_file_path,
+        sheet = sheet,
+        skip = skip
+    ) |>
+        .select_cols(column_patterns) |>
+        .rename_co_columns() |>
+        .convert_site_id_poc_column_types() |>
+        .drop_rows_all_na() |>
+        dplyr::distinct()
 }
 
+.get_all_co_monitors <- function(excel_file_path, sheets, skip, column_patterns) {
+    co_8h <- .get_initial_co_monitors(
+        excel_file_path = excel_file_path,
+        sheet = sheets$co_8h_monitors,
+        skip = skip,
+        column_patterns = column_patterns$monitors
+    )
+
+    co_1h <- .get_initial_co_monitors(
+        excel_file_path = excel_file_path,
+        sheet = sheets$co_1h_monitors,
+        skip = skip,
+        column_patterns = column_patterns$monitors
+    )
+
+    list(
+        co_8h = co_8h,
+        co_1h = co_1h
+    )
+}
+
+.get_initial_co_sites <- function(excel_file_path, sheet, skip, column_patterns) {
+    .import_regulatory_monitor_data(
+        excel_file_path = excel_file_path,
+        sheet = sheet,
+        skip = skip
+    ) |>
+        .select_cols(column_patterns) |>
+        .rename_co_columns() |>
+        .make_aqs_site_id_char() |>
+        .drop_rows_all_na() |>
+        dplyr::distinct()
+}
+
+.get_all_co_sites <- function(excel_file_path, sheets, skip, column_patterns) {
+    co_8h <- .get_initial_co_sites(
+        excel_file_path = excel_file_path,
+        sheet = sheets$co_8h_sites,
+        skip = skip,
+        column_patterns = column_patterns$sites
+    )
+
+    co_1h <- .get_initial_co_sites(
+        excel_file_path = excel_file_path,
+        sheet = sheets$co_1h_sites,
+        skip = skip,
+        column_patterns = column_patterns$sites
+    )
+
+    list(
+        co_8h = co_8h,
+        co_1h = co_1h
+    )
+}
+
+.get_initial_co_data <- function(year, setup_definitions) {
+    # Import monitor data for all years
+    monitors <- .get_all_co_monitors(
+        excel_file_path = setup_definitions$file_definitions$excel_file_path,
+        sheets = setup_definitions$sheets,
+        skip = setup_definitions$skip,
+        column_patterns = setup_definitions$column_patterns
+    )
+
+    if (year %in% 2014:2023) {
+        list(monitors = monitors)
+    } else if (year %in% 2012:2013) {
+        sites <- .get_all_co_sites(
+            excel_file_path = setup_definitions$file_definitions$excel_file_path,
+            sheets = setup_definitions$sheets,
+            skip = setup_definitions$skip,
+            column_patterns = setup_definitions$column_patterns
+        )
+
+        list(
+            monitors = monitors,
+            sites = sites
+        )
+    }
+}
+
+# Validity determination --------------------------------------------------------
 .filter_co_valid_dv <- function(df) {
     df |>
         dplyr::filter(!is.na(valid_dv))
 }
 
-.get_co_monitors <- function(year) {
-    temp_directory <- .create_temp_subdirectory("co")
-    url <- .define_co_url(year = year)
-    excel_file_path <- .download_regulatory_monitor_data(
-        year = year,
-        url = url,
-        temp_directory = temp_directory
-    )
-    sheet_8h_monitors <- .define_co_8h_monitors_sheet(year = year)
-    sheet_1h_monitors <- .define_co_1h_monitors_sheet(year = year)
-    monitor_column_patterns <- .define_co_column_patterns(year = year, level = "monitor")
-
-    co_8h <- .import_co_data(
-        year = year,
-        excel_file_path = excel_file_path,
-        sheet = sheet_8h_monitors
-    ) |>
-        .select_cols(monitor_column_patterns) |>
-        .rename_co_columns() |>
-        .make_aqs_site_id_char()
-
-    co_1h <- .import_co_data(
-        year = year,
-        excel_file_path = excel_file_path,
-        sheet = sheet_1h_monitors
-    ) |>
-        .select_cols(monitor_column_patterns) |>
-        .rename_co_columns() |>
-        .make_aqs_site_id_char()
-
-    if (year %in% c(2012, 2013)) {
-        sheet_8h_sites <- .define_co_8h_sites_sheet(year)
-        sheet_1h_sites <- .define_co_1h_sites_sheet(year)
-        site_column_patterns <- .define_co_column_patterns(year = year, level = "site")
-        co_8h_sites <- .import_co_data(
-            year = year,
-            excel_file_path = excel_file_path,
-            sheet = sheet_8h_sites
-        ) |>
-            .select_cols(site_column_patterns) |>
-            .rename_co_columns() |>
-            .make_aqs_site_id_char()
-
-        co_1h_sites <- .import_co_data(
-            year = year,
-            excel_file_path = excel_file_path,
-            sheet = sheet_1h_sites
-        ) |>
-            .select_cols(site_column_patterns) |>
-            .rename_co_columns() |>
-            .make_aqs_site_id_char()
-
-        co_8h <- co_8h |>
-            dplyr::right_join(co_8h_sites, by = "aqs_site_id")
-        co_1h <- co_1h |>
-            dplyr::right_join(co_1h_sites, by = "aqs_site_id")
-    }
-
-    co <- .combine_co_standards(co_8h, co_1h) |>
+.get_unique_valid_co_monitors <- function(df) {
+    df |>
         .filter_co_valid_dv() |>
         dplyr::select("aqs_site_id", "poc") |>
+        dplyr::distinct()
+}
+
+.get_co_unique_valid_sites <- function(df) {
+    df |>
+        .filter_co_valid_dv() |>
+        dplyr::select("aqs_site_id") |>
+        dplyr::distinct()
+}
+
+.determine_co_validity <- function(year, initial_data_list) {
+    if (year %in% 2014:2023) {
+        list(
+            co_8h = .get_unique_valid_co_monitors(initial_data_list$monitors$co_8h),
+            co_1h = .get_unique_valid_co_monitors(initial_data_list$monitors$co_1h)
+        )
+    } else if (year %in% 2012:2013) {
+        list(
+            co_8h = .get_co_unique_valid_sites(initial_data_list$sites$co_8h),
+            co_1h = .get_co_unique_valid_sites(initial_data_list$sites$co_1h)
+        )
+    }
+}
+
+# DF combination ----------------------------------------------------------------
+# For 2012-2013, sites and monitors must be joined
+# DFs for each standard are bound into single DF for all years
+
+.join_co_sites_and_monitors <- function(valid_data_list, initial_data_list) {
+    co_8h <- dplyr::left_join(valid_data_list$co_8h, initial_data_list$monitors$co_8h, by = "aqs_site_id")
+    co_1h <- dplyr::left_join(valid_data_list$co_1h, initial_data_list$monitors$co_1h, by = "aqs_site_id")
+    list(
+        co_8h = co_8h,
+        co_1h = co_1h
+    )
+}
+
+.combine_co_data <- function(year, initial_data_list, valid_data_list) {
+    if (year %in% 2014:2023) {
+        rbind(valid_data_list$co_8h, valid_data_list$co_1h) |>
+            dplyr::distinct()
+    } else if (year %in% 2012:2013) {
+        joined_list <- .join_co_sites_and_monitors(
+            valid_data_list = valid_data_list,
+            initial_data_list = initial_data_list
+        )
+        rbind(joined_list$co_8h, joined_list$co_1h) |>
+            dplyr::distinct()
+    }
+}
+
+# Main function ----------------------------------------------------------------
+.get_co_monitors <- function(year) {
+    setup <- .set_up_co_processing(year = year)
+    initial_data <- .get_initial_co_data(year = year, setup_definitions = setup)
+    valid_data <- .determine_co_validity(year = year, initial_data_list = initial_data)
+    co <- .combine_co_data(year = year, initial_data_list = initial_data, valid_data_list = valid_data) |>
         dplyr::mutate(
-            poc = as.integer(poc),
             year = year,
             parameter_name = "Carbon monoxide"
         )
-
-    unlink(temp_directory, recursive = TRUE)
+    unlink(setup$file_definitions$temp_directory, recursive = TRUE)
     co
 }
--- tests/testthat/test-regulatory_monitors_co.R
+++ tests/testthat/test-regulatory_monitors_co.R
@@ -1,3 +1,4 @@
+# Set up -----------------------------------------------------------------------
 test_that("CO URL is defined correctly and has active link", {
   year <- sample(2005:2023, 1)
   url <- .define_co_url(year)
@@ -5,58 +6,57 @@ test_that("CO URL is defined correctly and has active link", {
   expect_true(httr::HEAD(url)$status == 200)
 })
 
-test_that("CO 8-hour monitor sheet is correctly defined", {
+test_that("C0 8hr monitor sheet is correctly defined", {
   years <- 2012:2023
   purrr::map(years, function(year) {
-    temp_directory <- .create_temp_subdirectory("co")
-    url <- .define_co_url(year)
-    excel_file_path <- .download_regulatory_monitor_data(year, url, temp_directory)
-    sheet <- .define_co_8h_monitors_sheet(year)
-    excel_sheets <- readxl::excel_sheets(excel_file_path)
-    expect_true(sheet %in% excel_sheets)
-    unlink(temp_directory, recursive = TRUE)
+    setup <- get(stringr::str_c("co_setup_", year))
+    excel_sheets <- readxl::excel_sheets(setup$file_definitions$excel_file_path)
+    expect_true(setup$sheets$co_8h_monitors %in% excel_sheets)
   })
 })
 
-test_that("CO 8-hour site sheet is correctly defined", {
-  years <- 2012:2013
-  purrr::map(years, function(year) {
-    temp_directory <- .create_temp_subdirectory("co")
-    url <- .define_co_url(year)
-    excel_file_path <- .download_regulatory_monitor_data(year, url, temp_directory)
-    sheet <- .define_co_8h_sites_sheet(year)
-    excel_sheets <- readxl::excel_sheets(excel_file_path)
-    expect_true(sheet %in% excel_sheets)
-    unlink(temp_directory, recursive = TRUE)
-  })
-})
-
-test_that("CO 1-hour monitor sheet is correctly defined", {
+test_that("CO 1hr monitor sheet is correctly defined", {
   years <- 2012:2023
   purrr::map(years, function(year) {
-    temp_directory <- .create_temp_subdirectory("co")
-    url <- .define_co_url(year)
-    excel_file_path <- .download_regulatory_monitor_data(year, url, temp_directory)
-    sheet <- .define_co_1h_monitors_sheet(year)
-    excel_sheets <- readxl::excel_sheets(excel_file_path)
-    expect_true(sheet %in% excel_sheets)
-    unlink(temp_directory, recursive = TRUE)
+    setup <- get(stringr::str_c("co_setup_", year))
+    excel_sheets <- readxl::excel_sheets(setup$file_definitions$excel_file_path)
+    expect_true(setup$sheets$co_1h_monitors %in% excel_sheets)
   })
 })
 
-test_that("CO 1-hour site sheet is correctly defined", {
+test_that("CO 8hr site sheet is correctly defined", {
   years <- 2012:2013
   purrr::map(years, function(year) {
-    temp_directory <- .create_temp_subdirectory("co")
-    url <- .define_co_url(year)
-    excel_file_path <- .download_regulatory_monitor_data(year, url, temp_directory)
-    sheet <- .define_co_1h_sites_sheet(year)
-    excel_sheets <- readxl::excel_sheets(excel_file_path)
-    expect_true(sheet %in% excel_sheets)
-    unlink(temp_directory, recursive = TRUE)
+    setup <- get(stringr::str_c("co_setup_", year))
+    excel_sheets <- readxl::excel_sheets(setup$file_definitions$excel_file_path)
+    expect_true(setup$sheets$co_8h_sites %in% excel_sheets)
   })
 })
 
+test_that("CO 1hr site sheet is correctly defined", {
+  years <- 2012:2013
+  purrr::map(years, function(year) {
+    setup <- get(stringr::str_c("co_setup_", year))
+    excel_sheets <- readxl::excel_sheets(setup$file_definitions$excel_file_path)
+    expect_true(setup$sheets$co_1h_sites %in% excel_sheets)
+  })
+})
+
+test_that("CO skip is correctly defined", {
+  years <- 2012:2023
+  purrr::map(years, function(year) {
+    setup <- get(stringr::str_c("co_setup_", year))
+    if (year == 2013) {
+      expect_equal(setup$skip, 2)
+    } else {
+      (
+        expect_equal(setup$skip, 3)
+      )
+    }
+  })
+})
+
+# Preliminary data processing --------------------------------------------------
 test_that("CO monitor columns are selected correctly", {
   df <- data.frame(
     site = 1:10,
@@ -82,10 +82,27 @@ test_that("CO monitor columns are selected correctly", {
   expect_equal(
     .select_cols(
       df,
-      col_patterns = "^site$|aqs_site_id|poc|^valid|x20[0-9]{2}_[0-9]_hour_design_value"
+      col_patterns = .define_co_monitor_column_patterns()
     ),
     expected_df
   )
+})
+
+test_that("Intermediate monitor DFs have correct columns", {
+  # 2014-2023 (monitor level valid_dv)
+  years <- 2014:2023
+  purrr::map(years, function(year) {
+    co_initial_data <- get(stringr::str_c("co_initial_data_", year))
+    expect_equal(colnames(co_initial_data$monitors$co_8h), c("aqs_site_id", "poc", "valid_dv"))
+    expect_equal(colnames(co_initial_data$monitors$co_1h), c("aqs_site_id", "poc", "valid_dv"))
+  })
+  # 2012-2013 (site level valid_dv)
+  years <- 2012:2013
+  purrr::map(years, function(year) {
+    co_initial_data <- get(stringr::str_c("co_initial_data_", year))
+    expect_equal(colnames(co_initial_data$monitors$co_8h), c("aqs_site_id", "poc"))
+    expect_equal(colnames(co_initial_data$monitors$co_1h), c("aqs_site_id", "poc"))
+  })
 })
 
 test_that("CO site columns are selected correctly", {
@@ -108,17 +125,27 @@ test_that("CO site columns are selected correctly", {
   expect_equal(
     .select_cols(
       df_2012,
-      col_patterns = "^site$|2012"
+      col_patterns = .define_co_site_column_patterns(year = 2012)
     ),
     expected_df_2012
   )
   expect_equal(
     .select_cols(
       df_2013,
-      col_patterns = "^site$|2013"
+      col_patterns = .define_co_site_column_patterns(year = 2013)
     ),
     expected_df_2013
   )
+})
+
+test_that("Intermediate site DFs have correct columns", {
+  # 2012-2013 only
+  years <- 2012:2013
+  purrr::map(years, function(year) {
+    co_initial_data <- get(stringr::str_c("co_initial_data_", year))
+    expect_equal(colnames(co_initial_data$sites$co_8h), c("aqs_site_id", "valid_dv"))
+    expect_equal(colnames(co_initial_data$sites$co_1h), c("aqs_site_id", "valid_dv"))
+  })
 })
 
 test_that("CO columns are renamed correctly", {
@@ -138,22 +165,7 @@ test_that("CO columns are renamed correctly", {
   )
 })
 
-test_that("CO standards are combined correctly", {
-  df_8h <- data.frame(
-    valid_dv = c("1", "2", "3")
-  )
-  df_1h <- data.frame(
-    valid_dv = c("4", "5", "6")
-  )
-  expected_df <- data.frame(
-    valid_dv = c(1, 2, 3, 4, 5, 6)
-  )
-  expect_equal(
-    .combine_co_standards(df_8h, df_1h),
-    expected_df
-  )
-})
-
+# Validity determination -------------------------------------------------------
 test_that("CO valid_dv is filtered correctly", {
   df <- data.frame(
     valid_dv = c(1, NA, 2, 3, NA, 4)
@@ -167,6 +179,185 @@ test_that("CO valid_dv is filtered correctly", {
   )
 })
 
+test_that("CO validity is determined correctly", {
+  # 2014-2023 (monitor level valid_dv)
+  years <- 2014:2023
+  purrr::map(years, function(year) {
+    co_initial_data <- get(stringr::str_c("co_initial_data_", year))
+    co_valid_data <- get(stringr::str_c("co_valid_data_", year))
+    expect_equal(
+      nrow(co_valid_data$co_8h),
+      nrow(co_initial_data$monitors$co_8h |>
+        dplyr::filter(!is.na(valid_dv)))
+    )
+    expect_equal(
+      nrow(co_valid_data$co_1h),
+      nrow(co_initial_data$monitors$co_1h |>
+        dplyr::filter(!is.na(valid_dv)))
+    )
+  })
+  # 2012-2013 (site level valid_dv)
+  years <- 2012:2013
+  purrr::map(years, function(year) {
+    co_initial_data <- get(stringr::str_c("co_initial_data_", year))
+    co_valid_data <- get(stringr::str_c("co_valid_data_", year))
+    expect_equal(
+      nrow(co_valid_data$co_8h),
+      nrow(co_initial_data$sites$co_8h |>
+        dplyr::filter(!is.na(valid_dv)))
+    )
+    expect_equal(
+      nrow(co_valid_data$co_1h),
+      nrow(co_initial_data$sites$co_1h |>
+        dplyr::filter(!is.na(valid_dv)))
+    )
+  })
+})
+
+# DF combination ----------------------------------------------------------------
+test_that("DFs have expected columns prior to join", {
+  years <- 2012:2013
+  purrr::map(years, function(year) {
+    co_initial_data <- get(stringr::str_c("co_initial_data_", year))
+    co_valid_data <- get(stringr::str_c("co_valid_data_", year))
+    expect_equal(
+      colnames(co_initial_data$monitors$co_8h),
+      c("aqs_site_id", "poc")
+    )
+    expect_equal(
+      colnames(co_initial_data$monitors$co_1h),
+      c("aqs_site_id", "poc")
+    )
+    expect_equal(
+      colnames(co_valid_data$co_8h),
+      c("aqs_site_id")
+    )
+    expect_equal(
+      colnames(co_valid_data$co_1h),
+      c("aqs_site_id")
+    )
+  })
+})
+
+test_that("No duplicates exist in joining fields", {
+  years <- 2012:2013
+  purrr::map(years, function(year) {
+    co_initial_data <- get(stringr::str_c("co_initial_data_", year))
+    co_valid_data <- get(stringr::str_c("co_valid_data_", year))
+    expect_true(!anyDuplicated(co_initial_data$monitors$co_8h))
+    expect_true(!anyDuplicated(co_initial_data$monitors$co_1h))
+    expect_true(!anyDuplicated(co_valid_data$co_8h))
+    expect_true(!anyDuplicated(co_valid_data$co_1h))
+  })
+})
+
+test_that("Joined DFs for 2012 and 2013 have expected number of rows", {
+  years <- 2012:2013
+  purrr::map(years, function(year) {
+    co_initial_data <- get(stringr::str_c("co_initial_data_", year))
+    co_valid_data <- get(stringr::str_c("co_valid_data_", year))
+    sites_unmatched_8h <- sum(!co_valid_data$co_8h$aqs_site_id %in% co_initial_data$monitors$co_8h$aqs_site_id)
+    matched_8h <- sum(co_initial_data$monitors$co_8h$aqs_site_id %in% co_valid_data$co_8h$aqs_site_id)
+    expect_equal(
+      nrow(.join_co_sites_and_monitors(co_valid_data, co_initial_data)$co_8h),
+      (sites_unmatched_8h + matched_8h)
+    )
+    sites_unmatched_1h <- sum(!co_valid_data$co_1h$aqs_site_id %in% co_initial_data$monitors$co_1h$aqs_site_id)
+    matched_1h <- sum(co_initial_data$monitors$co_1h$aqs_site_id %in% co_valid_data$co_1h$aqs_site_id)
+    expect_equal(
+      nrow(.join_co_sites_and_monitors(co_valid_data, co_initial_data)$co_1h),
+      (sites_unmatched_1h + matched_1h)
+    )
+  })
+})
+
+test_that("Final combined DF has correct number of unique monitors", {
+  # 2014-2023
+  years <- 2014:2023
+  purrr::map(years, function(year) {
+    co_initial_data <- get(stringr::str_c("co_initial_data_", year))
+    co_valid_data <- get(stringr::str_c("co_valid_data_", year))
+    expect_equal(
+      (nrow(dplyr::inner_join(co_valid_data$co_8h, co_valid_data$co_1h, by = c("aqs_site_id", "poc"))) +
+        nrow(dplyr::anti_join(co_valid_data$co_8h, co_valid_data$co_1h, by = c("aqs_site_id", "poc")))),
+      nrow(.combine_co_data(year, co_initial_data, co_valid_data))
+    )
+  })
+  # 2012-2013
+  years <- 2012:2013
+  purrr::map(years, function(year) {
+    co_initial_data <- get(stringr::str_c("co_initial_data_", year))
+    co_valid_data <- get(stringr::str_c("co_valid_data_", year))
+    joined_data <- .join_co_sites_and_monitors(co_valid_data, co_initial_data)
+    expect_equal(
+      (nrow(dplyr::inner_join(joined_data$co_8h, joined_data$co_1h, by = c("aqs_site_id", "poc"))) +
+        nrow(dplyr::anti_join(joined_data$co_8h, joined_data$co_1h, by = c("aqs_site_id", "poc")))),
+      nrow(.combine_co_data(year, co_initial_data, co_valid_data))
+    )
+  })
+})
+
+test_that(".combine_co_data joins data correctly", {
+  # 2014-2023
+  df_1 <- data.frame(
+    aqs_site_id = c(1, 2, 3),
+    poc = c(1, 2, 3)
+  )
+  df_2 <- data.frame(
+    aqs_site_id = c(3, 4, 5),
+    poc = c(3, 4, 5)
+  )
+  valid_data_list <- list(
+    co_8h = df_1,
+    co_1h = df_2
+  )
+  expected_df <- data.frame(
+    aqs_site_id = c(1, 2, 3, 4, 5),
+    poc = c(1, 2, 3, 4, 5)
+  )
+  expect_equal(
+    .combine_co_data(year = sample(2014:2023, 1), initial_data_list = NULL, valid_data_list),
+    expected_df
+  )
+  # 2012-2013
+  # Don't drop sites without corresponding monitors
+  # Include all monitors with corresponding sites
+  # Drop duplicate monitors when binding
+  sites_1 <- data.frame(
+    aqs_site_id = c(1, 2, 3, 6)
+  )
+  sites_2 <- data.frame(
+    aqs_site_id = c(3, 4, 5)
+  )
+  valid_data_list <- list(
+    co_8h = sites_1,
+    co_1h = sites_2
+  )
+  monitors_1 <- data.frame(
+    aqs_site_id = c(1, 2, 2, 3),
+    poc = c(1, 2, 1, 3)
+  )
+  monitors_2 <- data.frame(
+    aqs_site_id = c(3, 4, 4),
+    poc = c(3, 4, 1)
+  )
+  initial_data_list <- list(
+    monitors = list(
+      co_8h = monitors_1,
+      co_1h = monitors_2
+    )
+  )
+  expected_df <- data.frame(
+    aqs_site_id = c(1, 2, 2, 3, 6, 4, 4, 5),
+    poc = c(1, 2, 1, 3, NA, 4, 1, NA)
+  )
+  expect_equal(
+    .combine_co_data(year = sample(2012:2013, 1), initial_data_list, valid_data_list),
+    expected_df
+  )
+})
+
+# Master function --------------------------------------------------------------
 test_that("CO master function returns expected data", {
   years <- 2012:2023
   purrr::map(years, function(year) {
@@ -174,5 +365,6 @@ test_that("CO master function returns expected data", {
     expect_true(
       all(c("aqs_site_id", "poc", "year", "parameter_name") %in% colnames(df))
     )
+    expect_equal(df, get(stringr::str_c("co_", year)))
   })
 })
